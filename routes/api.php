52f08a77737c8dd9138f906fc281d27595c79cb0
mehrdad kalateh arabi <mehrdad1234kalateh@gmail.com>
2025-07-14 15:29:58 +0330

    routes added

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,26 @@
 <?php
+    use App\Http\Controllers\ProvinceCitiesController;
+    use App\Http\Controllers\SaleController;
+    use App\Http\Controllers\TicketController;
+    use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\CPerson;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\SUserC;
-use App\Models\father;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    Route::prefix("tickets")->group(function(){
+        Route::post("sendTicket", [TicketController::class, "store"]);
+        Route::get("fetchTickets", [TicketController::class, "fetch"]);
+        Route::patch("updateStatus", [TicketController::class, "updateStatus"]);
+    });
 
+    Route::prefix("officers")->group(function(){
+        Route::post("register", [SaleController::class, "register"]);
+        Route::get("saleRequests", [SaleController::class, 'fetchData']);
+        Route::patch("updateStatus", [SaleController::class, "updateStatus"]);
+        Route::delete("saleRefer", [SaleController::class, "refer"]);
+    });
 
+    Route::prefix("regions")->group(function(){
+        Route::post("register", [ProvinceCitiesController::class, "register"]);
+        Route::get("fetchProvinces", [ProvinceCitiesController::class, "fetchProvince"]);
+        Route::patch("updateProvinces", [ProvinceCitiesController::class, "updateProvince"]);
+        Route::delete("deleteProvinces", [ProvinceCitiesController::class, "deleteProvince"]);
+    });
 
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get("/reg", [CPerson::class, "register"]);
-Route::get("/getData/{id}" , [CPerson::class, "getData"])->middleware('auth:sanctum');
-
-Route::post("/register",[PostController::class, "register"])->middleware('auth:sanctum');
-Route::get("/getdata", [PostController::class, "read"])->middleware('auth:sanctum');
-
-Route::post('/register', [SUserC::class, 'register']);
-Route::post('/login', [SUserC::class, 'login']);
-
-Route::get('/unauthenticated', [SUserC::class, 'unauthenticated'])->name("login");
